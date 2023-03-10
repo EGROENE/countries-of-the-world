8631e28b1915eda7fda4d4be6ee3f0f38fabe134
Ethan <ethangroene@gmail.com>
2023-03-09 19:33:05 -0600

    Add functionality for  light/dark theme switching

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+// Toggle light/dark theme:
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
+
+const enableDarkMode = () => {
+  // add dark class to body
+  document.body.classList.add("dark-mode");
+  // update dark mode in localStorage
+  localStorage.setItem("darkMode", "enabled");
+  // 'check' the slider:
+  document.getElementById("dark-mode-toggle").checked = true;
+  // make sun logo more opaque:
+  sun.style.opacity = "0.4";
+  // remove moon's opacity:
+  moon.style.opacity = "1";
+};
+
+const disableDarkMode = () => {
+  // remove dark class to body
+  document.body.classList.remove("dark-mode");
+  // update dark mode in localStorage
+  localStorage.setItem("darkMode", "disabled");
+  // 'uncheck' the slider:
+  document.getElementById("dark-mode-toggle").checked = false;
+  // make moon logo more opaque:
+  moon.style.opacity = "0.4";
+  // remove sun's opacity:
+  sun.style.opacity = "1";
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+// Get info from API, populate site:
 const apiURL = "https://restcountries.com/v3.1/all";
 
 async function getCountries() {
@@ -42,6 +88,15 @@ async function getCountries() {
       "<header>" +
       country.name.common +
       "</header>" +
+      "<p>Region: " +
+      country.region +
+      "</p>" +
+      "<p>Population: " +
+      country.population.toLocaleString() +
+      "</p>" +
+      "<p>Capital: " +
+      country.capital.toString() +
+      "</p>" +
       "</a>";
   }
 }
