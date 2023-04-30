d1b15c53152ae221bcd6fa2bd0d897ecfc91af10
Ethan <ethangroene@gmail.com>
2023-04-29 21:40:53 -0600

    Put countries into condensed array, pop homepage w/ this

--- js/main.js
+++ js/main.js
@@ -23,8 +23,10 @@ async function populateHomepage() {
     return 0;
   });
 
+  console.log(mainArray);
+
   for (let country of mainArray) {
-    // Get native names into array, then use that array to add all native names to data-native-names (if country has native names):
+    /* // Get native names into array, then use that array to add all native names to data-native-names (if country has native names):
     let nativeNamesDataset = "NONE";
     let nativeNamesLowercase;
     if (country.name.nativeName) {
@@ -50,7 +52,7 @@ async function populateHomepage() {
     }
 
     // Set country's currency dataset value:
-    let currencyDataset = "NONE";
+    //let currencyDataset = "NONE";
     if (country.currencies) {
       let currencies = Object.values(country.currencies);
       let currencyNames = currencies.map((currency) =>
@@ -140,70 +142,222 @@ async function populateHomepage() {
         linkTitle = "Click to learn more about the " + country.name.official;
       } else {
         linkTitle = "Click to learn more about the " + country.name.common;
-      }
+      } */
+  }
+  // Put remaining code into one function outside of larger function. Loop through condensed array, not mainArray, to pop homepage. Just keep += new elements that appear in condensed array into the inner html
+  // This new func should be called on pageload and upon click of "show more" btn
+  // Actually, should be part of populateHomepage async function
+  let startingIndex = -1;
+  const condensedArray = [];
+  let homepageDOM = document.getElementById("homepage-container");
+  // Begin function here
+  const populateHomepageHTML = () => {
+    startingIndex++;
+    console.log(homepageDOM.children[2]);
+    for (let i = startingIndex; i < 25; i++) {
+      condensedArray.push(mainArray[i]);
+    }
+    console.log(condensedArray);
 
     // Populate homepage:
+    // Loop through condensedArray to do this.
     let allCountriesArea = document.getElementById("all-countries-container");
-    allCountriesArea.innerHTML +=
-      '<a href="./country-pages/' +
-      country.name.common
-        .toLowerCase()
-        .replace(/[$,]/g, "")
-        .replace(/\s/g, "-") +
-      '.html" class="country-card" title="' +
-      linkTitle +
-      '!" data-full-name=' +
-      country.name.official.toLowerCase().replace(/\s/g, "-") +
-      " data-common-name=" +
-      country.name.common.toLowerCase().replace(/\s/g, "-") +
-      " data-native-names=" +
-      nativeNamesDataset +
-      " data-region=" +
-      country.region.toLowerCase().replace(/\s/g, "-") +
-      " data-subregion=" +
-      subregion.toLowerCase().replace(/\s/g, "-") +
-      " data-capital=" +
-      capitalDataset +
-      " data-population=" +
-      String(country.population) +
-      " data-currencies=" +
-      currencyDataset +
-      " data-languages=" +
-      languagesDataset +
-      ">" +
-      "<div class='card-flag-container'>" +
-      "<img id ='" +
-      country.name.common.toLowerCase().replace(/\s/g, "-") +
-      "-flag-homepage' src='" +
-      country.flags.png +
-      "' alt='" +
-      country.flags.alt +
-      "'>" +
-      "</div>" +
-      "<div class='country-card-text-container'>" +
-      "<header>" +
-      country.name.common +
-      "</header>" +
-      "<p>Region: " +
-      "<span>" +
-      country.region +
-      "</span>" +
-      "</p>" +
-      "<p>Subregion: " +
-      "<span>" +
-      subregion +
-      "</span>" +
-      "</p>" +
-      "<p>Population: " +
-      "<span>" +
-      country.population.toLocaleString() +
-      "</span>" +
-      "</p>" +
-      capitalElement +
-      languagesElement +
-      "</div>" +
-      "</a>";
-  }
+    // If length of condensedArray === mainArray, then show more btn variable should be blank. Else, var should be HTML for a button.
+    let showMoreBtn = document.createElement("button");
+    showMoreBtn.setAttribute("id", "show-more-button");
+    showMoreBtn.innerHTML += "Show more";
+    console.log(showMoreBtn);
+
+    for (let country of condensedArray) {
+      // Get native names into array, then use that array to add all native names to data-native-names (if country has native names):
+      let nativeNamesDataset = "NONE";
+      let nativeNamesLowercase;
+      if (country.name.nativeName) {
+        // Get array of objects, each object pertaining to country's native name in a certain language:
+        let namesInLanguage = Object.values(country.name.nativeName);
+        // Initialize array that will contain array(s) of native names (w/o keys that are present in namesInLanguage):
+        let nativeNames = [];
+        // Initialize variable that will be the flattened version of nativeNames:
+        let nativeNamesFlat;
+        // For every name in namesInLanguage, push its flattened values to nativeNames, then assign nativeNamesFlat the value of nativeNames.flat():
+        for (let name of namesInLanguage) {
+          nativeNames.push(Object.values(name).flat());
+          nativeNamesFlat = nativeNames.flat();
+        }
+        // Create an array of all the names in nativeNamesFlat in lowercase form:
+        nativeNamesLowercase = nativeNamesFlat.map((name) =>
+          name.toLowerCase()
+        );
+        // Assign nativeNamesDataset to an array of the values of nativeNamesLowerCase, in which all spaces have been replaced by hyphens:
+        nativeNamesDataset = nativeNamesLowercase.map((name) =>
+          name.replace(/\s/g, "-")
+        );
+        // Convert array to a string, joining elements w/ a hyphen:
+        nativeNamesDataset = nativeNamesDataset.join("-");
+      }
+
+      // Set country's currency dataset value:
+      //let currencyDataset = "NONE";
+      if (country.currencies) {
+        let currencies = Object.values(country.currencies);
+        let currencyNames = currencies.map((currency) =>
+          currency.name.toLowerCase()
+        );
+        currencyDataset = currencyNames.map((currencyName) =>
+          currencyName.replace(/\s/g, "-")
+        );
+      }
+
+      let subregion = "NONE";
+      if (country.subregion) {
+        subregion = country.subregion;
+      }
+
+      // Get capital(s):
+      let capitalDataset = "NONE";
+      let capitalElement = "<p>Capital: <span>NONE</span></p>";
+      if (country.capital) {
+        capital = country.capital;
+        // Get dataset:
+        capitalDataset = capital.map((capital) => capital.toLowerCase());
+        capitalDataset = capitalDataset.join("-").replace(/\s/g, "-");
+        // If more than one capital, adjust grammar accordingly:
+        capital.length > 1
+          ? (capitalElement =
+              "<p>Capitals: <span>" + capital.join(", ") + "</span></p>")
+          : (capitalElement = "<p>Capital: <span>" + capital + "</span></p>");
+      }
+
+      // Get language(s):
+      let languagesDataset = "NONE";
+      let languagesElement = "<p>Languages: <span>NONE</span></p>";
+      if (country.languages) {
+        languages = Object.values(country.languages);
+        // Set country's languages dataset value:
+        languagesDataset = languages.map((language) => language.toLowerCase());
+        languagesDataset = languagesDataset.join("-").replace(/\s/g, "-");
+        // If more than one language, adjust grammar accordingly:
+        languages.length > 1
+          ? (languagesElement =
+              "<p>Languages: <span>" + languages.join(", ") + "</span></p>")
+          : (languagesElement =
+              "<p>Language: <span>" + languages + "</span></p>");
+        //languages = languages.join(", ");
+      }
+
+      // If names sound better with 'the' in front of them, add 'the' to these countries' link titles. Else, link title is country's common name.
+      let linkTitle = "Click to learn more about " + country.name.common;
+      const countriesThatSoundBetterWithTheBeforeName = [
+        "caribbean-netherlands",
+        "falkland-islands",
+        "faroe-islands",
+        "cocos-(keeling)-islands",
+        "cayman-islands",
+        "cook-islands",
+        "solomon-islands",
+        "turks-and-caicos-islands",
+        "isle-of-man",
+        "united-kingdom",
+        "netherlands",
+        "bahamas",
+        "republic-of-the-congo",
+        "dr-congo",
+        "british-indian-ocean-territory",
+        "british-virgin-islands",
+        "central-african-republic",
+        "french-southern-and-antarctic-lands",
+        "marshall-islands",
+        "maldives",
+        "northern-mariana-islands",
+        "pitcairn-islands",
+        "united-states",
+        "united-states-minor-outlying-islands",
+        "united-states-virgin-islands",
+        "åland-islands",
+      ];
+      if (
+        countriesThatSoundBetterWithTheBeforeName.includes(
+          country.name.common
+            .toLowerCase()
+            .replace(/[$,]/g, "")
+            .replace(/\s/g, "-")
+        )
+      )
+        if (country.name.common === "DR Congo") {
+          linkTitle = "Click to learn more about the " + country.name.official;
+        } else {
+          linkTitle = "Click to learn more about the " + country.name.common;
+        }
+
+      // Populate homepage:
+      allCountriesArea.innerHTML +=
+        '<a href="./country-pages/' +
+        country.name.common
+          .toLowerCase()
+          .replace(/[$,]/g, "")
+          .replace(/\s/g, "-") +
+        '.html" class="country-card" title="' +
+        linkTitle +
+        '!" data-full-name=' +
+        country.name.official.toLowerCase().replace(/\s/g, "-") +
+        " data-common-name=" +
+        country.name.common.toLowerCase().replace(/\s/g, "-") +
+        " data-native-names=" +
+        nativeNamesDataset +
+        " data-region=" +
+        country.region.toLowerCase().replace(/\s/g, "-") +
+        " data-subregion=" +
+        subregion.toLowerCase().replace(/\s/g, "-") +
+        " data-capital=" +
+        capitalDataset +
+        " data-population=" +
+        String(country.population) +
+        " data-currencies=" +
+        currencyDataset +
+        " data-languages=" +
+        languagesDataset +
+        ">" +
+        "<div class='card-flag-container'>" +
+        "<img id ='" +
+        country.name.common.toLowerCase().replace(/\s/g, "-") +
+        "-flag-homepage' src='" +
+        country.flags.png +
+        "' alt='" +
+        country.flags.alt +
+        "'>" +
+        "</div>" +
+        "<div class='country-card-text-container'>" +
+        "<header>" +
+        country.name.common +
+        "</header>" +
+        "<p>Region: " +
+        "<span>" +
+        country.region +
+        "</span>" +
+        "</p>" +
+        "<p>Subregion: " +
+        "<span>" +
+        subregion +
+        "</span>" +
+        "</p>" +
+        "<p>Population: " +
+        "<span>" +
+        country.population.toLocaleString() +
+        "</span>" +
+        "</p>" +
+        capitalElement +
+        languagesElement +
+        "</div>" +
+        "</a>";
+    }
+    console.log(homepageDOM.children[3]);
+    console.log(showMoreBtn);
+    homepageDOM.insertBefore(showMoreBtn, homepageDOM.children[4]);
+  };
+
+  populateHomepageHTML();
+
+  // Add EL for populateHomepageHTML to "show more" button
 
   // Add EL to scroll-to-top button. This EL's callback function scrolls to the top of the page when the button is clicked:
   const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
@@ -244,7 +398,6 @@ async function populateHomepage() {
     '<div id="dropdown-content"></div>';
 
   // Once all countries have displayed, hide loading message, which displays on pageload:
-  let homepageDOM = document.getElementById("homepage-container");
   homepageDOM.removeChild(homepageDOM.children[0]);
 
   // Add quiz link to homepage DOM:
